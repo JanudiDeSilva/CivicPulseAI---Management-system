40606bb7618a45ffe1a94ce9ba4a48fb4babaad5
IT24102877 <dgalappaththi.20@gmail.com>
2026-08-08 16:52:10 +0530

    Update db.js

--- backend-express/src/config/db.js
+++ backend-express/src/config/db.js
@@ -4,6 +4,12 @@ dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     logging: false,
 });
 
